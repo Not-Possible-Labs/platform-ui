abe1e821fa3d8e0327dc7efd4c99d2203ad7dfb9
Kaise White <kaisewhite@gmail.com>
2025-04-13 20:53:25 -0400

    Simplify theme provider imports and remove unused onClose prop from mobile nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
               onClick={() => setMobileNavOpen(!mobileNavOpen)}
             />
           </MobileNavHeader>
-          <MobileNavMenu isOpen={mobileNavOpen} onClose={() => setMobileNavOpen(false)}>
+          <MobileNavMenu isOpen={mobileNavOpen}>
             <div className="flex flex-col gap-2">
               {navItems.map((item, idx) => (
                 <a
